9bdbfd03259cea9d834a2a3498f18a044ad91b4e
uizzuu <psypsy1346@naver.com>
2025-08-21 12:35:31 +0900

    프로젝트 초기 생성

---
+++ src/main/java/com/my/jpaTest/dto/Gender.java
@@ -0,0 +1,6 @@
+package com.my.jpaTest.dto;
+
+
+public enum Gender {
+    Male, Female
+}
---
+++ src/main/java/com/my/jpaTest/entity/UserTest.java
@@ -0,0 +1,23 @@
+package com.my.jpaTest.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+// 테스트 코드 작성 단축기 : Ctrl + Shift + T
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+// BaseEntity의 ToString 호출
+@ToString(callSuper = true)
+public class UserTest extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+}
