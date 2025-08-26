4e1d9093e3a1daffe4adb8960c5a78ab151a1764
uizzuu <psypsy1346@naver.com>
2025-08-26 12:03:40 +0900

    양방향 연관관계 설정

--- src/main/java/com/my/jpaTest/entity/Member.java
+++ src/main/java/com/my/jpaTest/entity/Member.java
@@ -2,6 +2,8 @@ package com.my.jpaTest.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.*;
 
 @Entity
@@ -13,4 +15,13 @@ public class Member {
     @Id
     private String memberId;
     private String name;
+
+    // 팀 정보를 갖는 연관관계 구성
+    // java에서의 연관관계 핵심 : 서로 정보를 나눠갖는다
+    // ManyToOne 에는 JoinColumn(name = "teamId")이 항상 따라다님
+    // 반대로 OneToMany team에는 list로 member내용 알고있으니까 JoinColumn 필요없음
+    // OneToMany(mappedBy = "member") 의 형식으로 씀
+    @ManyToOne
+    @JoinColumn(name = "teamId") // 외래키(foreign key)
+    private Team team;
 }
