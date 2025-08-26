e1ce8af239e52a2c844a9645b191efdbbbe7d479
uizzuu <psypsy1346@naver.com>
2025-08-26 10:02:56 +0900

    덮어쓰기

---
+++ src/main/java/com/my/jpaTest/entity/Team.java
@@ -0,0 +1,17 @@
+package com.my.jpaTest.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Team {
+    @Id
+    private String teamId;
+    private String teamName;
+}
