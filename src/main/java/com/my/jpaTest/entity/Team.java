3b17f8b1d16d4e392e3a5f5fa9c3eed94e372e6d
uizzuu <psypsy1346@naver.com>
2025-08-26 12:03:40 +0900

    양방향 연관관계 설정

--- src/main/java/com/my/jpaTest/entity/Team.java
+++ src/main/java/com/my/jpaTest/entity/Team.java
@@ -1,17 +1,29 @@
 package com.my.jpaTest.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class Team {
     @Id
-    private String teamId;
+    private String teamId; // 외래키(foreign key)
     private String teamName;
+
+    // 팀에 소속된 멤버의 리스트를 저장
+    // member와 양방향 연관관계 설정 : Member 클래스의 Team 정보를 가리킴
+    @OneToMany(mappedBy = "team", fetch = FetchType.EAGER) // Member에 저장된 Team team을 가리킴
+    private List<Member> memberList = new ArrayList<>();
 }
---
+++ src/test/java/com/my/jpaTest/service/RelationTestServiceTest.java
@@ -0,0 +1,70 @@
+package com.my.jpaTest.service;
+
+import com.my.jpaTest.entity.Member;
+import com.my.jpaTest.entity.Team;
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.properties")
+class RelationTestServiceTest {
+    @Autowired
+    RelationTestService relationTestService;
+
+    @Autowired
+    EntityManager em;
+
+    @Test
+    @DisplayName("MemberAndTeam Insert Test")
+    void insertTest() {
+        // relationTestService.insertMemberAndTeam();
+    }
+
+    // 장원영(key: jang)의 팀이름 검색
+    // 연관관계가 없으면 한 없이 찾아들어가서 원하는 자료를 찾아야 함.
+    // 따라서 외래키 걸어줘야함 (연관관계 매핑)
+    @Test
+    @DisplayName("멤버 아이디로 팀 이름 찾기 테스트")
+    void findTeamName() {
+        // 멤버에서 memberId 찾기
+        Member jang = em.find(Member.class, "jang");
+        // 찾아온 멤버의 teamId team 에서 검색
+        Team team = em.find(Team.class, jang.getTeam().getTeamName());
+        System.out.println("Team Name : " + team);
+    }
+
+    @Test
+    @DisplayName("단방향 연관관계 설정 후 팀 이름  찾기")
+    void findTeam() {
+        // 저장 서비스 호출
+        // relationTestService.insertMemberAndTeam();
+        // memberId 정보 가져오기
+        Member jang = em.find(Member.class, "jang");
+        // 팀 정보 출력
+        System.out.println(jang.getTeam().getTeamName());
+    }
+
+    @Test
+    @DisplayName("양방향 연관관계 설정 후 저장하기")
+    void saveBothData() {
+        // relationTestService.insertBothRelation();
+    }
+
+    @Test
+    @DisplayName("씨름팀 멤버 출력")
+    void 씨름팀멤버출력() {
+        Team team = em.find(Team.class, "ssirum");
+        List<Member> members = team.getMemberList();
+        for (Member m : members) {
+            System.out.println(m.getName());
+        }
+    }
+}
